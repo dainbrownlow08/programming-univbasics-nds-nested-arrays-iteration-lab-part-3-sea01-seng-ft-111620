4841eaef0735613e5c0593d09d80b17c0166d066
Dain Brownlow <dain.brownlow@outlook.com>
2020-11-01 01:10:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,10 @@
 def join_nested_strings(src)
+  final_string = ""
   row_index = 0
   while row_index < src.count do
     column_index = 0
     while column_index < src[row_index].count do
-      
+      if src[]
     end
   end
 end
